f65cfc80d0fc8a6e39ac5b39b97b09cac76efc3d
AratiMhaske30 <aaratimhaske601@gmail.com>
2026-06-17 10:57:35 -0800

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Root',
-    database:'dental'
+    host:'bsfw181ajez1hg3uquvl-mysql.services.clever-cloud.com',
+    user:'uf9ccycrv1wiw2vz',
+    password:'uOHCAuYdpPZX3bYfzPM0',
+    database:'bsfw181ajez1hg3uquvl'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
